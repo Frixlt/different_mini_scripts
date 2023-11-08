f757a42e3465c599ffcbfd5bfc26a01d76abadaf
Frixit <57772236+Frixlt@users.noreply.github.com>
2023-11-08 22:18:45 +0300

    Update massa.py

--- massa.py
+++ massa.py
@@ -1,15 +1,2 @@
 import random
-
-size = 10
-array = [random.uniform(-10, 10) for i in range(size)]
-print(array)
-
-min_pos = float('inf')
-for x in array:
-    if x > 0 and x < min_pos:
-        min_pos = x
-
-if min_pos != float('inf'):
-    print("Минимальный положительный элемент:", min_pos)
-else:
-    print("Положительных элементов нет")
+print(f"Минимальный положительный элемент: {min([num for num in [random.uniform(-10, 10) for _ in range(10)] if num > 0])}")
